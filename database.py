bab11a020663382de00017739bf67914b2a1f959
timo <test@test.com>
2020-12-03 11:12:53 +0100

    for db class inserted init of cursor and table func

--- database.py
+++ database.py
@@ -5,6 +5,12 @@ class MySqliteDb(object):
     def __init__(self):
         self.test_var = 1
         self.connection = None
+        self.cursor = None
+        self.logging_table = False
+
+        self._create_connection()
+        self._create_cursor()
+
 
     def __del__(self):
         try:
@@ -12,22 +18,44 @@ class MySqliteDb(object):
         except Exception as e:
             raise e
 
-    def create_connection(self, db_file):
+    def _create_connection(self, db_file):
         """ Create a connection to the sqlite database"""
 
-        connection = None
+        # connection = None
 
         try:
-            connection = sqlite3.connect(db_file)
+            self.connection = sqlite3.connect(db_file)
             print("Successfully connected to sqlite3")
         except sqlite3.Error as e:
             raise e
-        finally:
-            if connection:
-                connection.close()
+
+        return self.connection
+
+    def _create_cursor(self):
+        """Creates Cursor object for sqlite db """
+        try:
+
+        except sqlite3.Error as e:
+            raise e
+
+    def create_logging_table(self, tablename):
+        """Creates the table for logging the html accesses"""
+        pass
+
+        #TODO: improve the check if table already exists by e.g. db query
+
+        if not self.logging_table:
+            # create the table
+            try:
+                cursor = self.connection.cursor()
+
+        else:
+            msg = f"table {tablename} already exists"
+            return msg
 
 
 if __name__ == '__main__':
     db = MySqliteDb()
     db.create_connection(
         'C:\\Users\\timok\\Desktop\\TOWA\\Python\\sqlite\\pysqlite.db')
+    db.create_logging_table("LoggingTable")
