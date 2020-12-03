ce3cd22c832c2058b31413e74962b39f4e539cfb
timo <test@test.com>
2020-12-03 09:56:24 +0100

    added request handler to http server

--- http_server.py
+++ http_server.py
@@ -1,10 +1,21 @@
 import http.server
 import socketserver
 
+
+class MyHttpRequestHandler(http.server.SimpleHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == "/index":
+            self.path = 'index.html'
+        return http.server.SimpleHTTPRequestHandler.do_GET(self)
+
+
+# create the handler obj
+handler_object = MyHttpRequestHandler
+
 PORT = 8000
 
 handler = http.server.SimpleHTTPRequestHandler
+myserver = socketserver.TCPServer(("", PORT), handler)
 
-with socketserver.TCPServer(("", PORT), handler) as httpd:
-    print("Server started at localhost: " + str(PORT))
-    httpd.serve_forever()
+# Start server
+myserver.serve_forever()
