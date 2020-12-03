a5c629075855d2c48f9066ff133a43cd58352e62
timo <test@test.com>
2020-12-03 13:26:21 +0100

    first func to log the client info

--- http_server.py
+++ http_server.py
@@ -2,36 +2,72 @@ import http.server
 import socketserver
 from urllib.parse import urlparse, parse_qs
 from database import MySqliteDb
+from datetime import datetime
 
 
 class MyHttpRequestHandler(http.server.SimpleHTTPRequestHandler):
+    def __init__(self, dbInstance: MySqliteDb):
+        self.db = dbInstance
+
     def do_GET(self):
+        # Log the data
+        self._insert_logdata()
 
-        #send ok response
-        self.send_response(200)
+        if self.path == "/":
+            #send ok response
+            self.send_response(200)
 
-        # setting the header
-        self.send_header("Content-type", "text/html")
+            # setting the header
+            self.send_header("Content-type", "text/html")
 
-        self.end_headers()
+            self.end_headers()
 
-        # extract query params
-        name = 'World'
-        query_components = parse_qs(urlparse(self.path).query)
-        if 'name' in query_components:
-            name = query_components['name'][0]
+            # extract query params
+            name = 'World'
+            query_components = parse_qs(urlparse(self.path).query)
+            if 'name' in query_components:
+                name = query_components['name'][0]
 
-        # get the ip adress of the client
-        client_address = self.client_address[0]
+            # get the headers and ip adress of the client
+            headers = self.headers
+            client_address = self.get_ip_client()
 
-        headers = self.headers
+            # create html code
+            html = f"<html><head></head><body><h1>Hello {name}! {client_address}  and the headers: {headers}</h1></body></html>"
 
-        # create html code
-        html = f"<html><head></head><body><h1>Hello {name}! {client_address}  and the headers: {headers}</h1></body></html>"
-
-        # write the html code
-        self.wfile.write(bytes(html, "utf8"))
+            # write the html code
+            self.wfile.write(bytes(html, "utf8"))
         return None
+
+    def get_ip_client(self):
+        # get the ip adress of the client
+        return self.client_address[0]
+
+    def _get_logging_data(self):
+        # get the ip adress of the client
+        user_ip = self.get_ip_client()
+        timestamp = int(datetime.utcnow().timestamp())
+        protocol = "dummy"
+        host = "host"
+        path = "path"
+        query = "query"
+
+        log_data = {
+            "user_ip": user_ip,
+            "timestamp": timestamp,
+            "protocol": protocol,
+            "host": host,
+            "path": path,
+            "query": query
+        }
+
+        print(log_data)
+        return log_data
+
+    def _insert_logdata(self):
+        log_data = self._get_logging_data()
+        self.db._insert_logging_data(log_data)
+        return log_data
 
 
 def myHttpServer():
@@ -41,7 +77,7 @@ def myHttpServer():
     db.create_logging_table("LoggingTable")
 
     # create the handler obj
-    handler_object = MyHttpRequestHandler
+    handler_object = MyHttpRequestHandler(db)
 
     PORT = 8000
 
